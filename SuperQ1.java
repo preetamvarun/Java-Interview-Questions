f3be55c74e04e016d4adb12cd37caf3bd9b79da0
PREETAM <preetamvarun99@gmail.com>
2023-11-03 11:57:27 +0000

    Completed First 3 Questions

--- SuperQ1.java
+++ SuperQ1.java
@@ -1,3 +1,32 @@
+/*
+    Super keyword allows us to refer to base class. There are three major use cases of
+    super keyword. 1) we can invoke base class constructor 2) we can call base class
+    methods. 3) we can access base class methods
+*/
 public class SuperQ1 {
+    public String Industry;
 
+    SuperQ1(String industryName){
+        this.Industry = industryName;
+    }
+
+    public void usage(){
+        System.out.println(this.Industry + " Helps to generate revenue");
+    }
+}
+
+class SubQ1 extends SuperQ1 {
+    SubQ1(String industryName){
+        super(industryName);
+    }
+
+    public void Msg(){
+        super.usage();
+        System.out.println("Industry in parent class is " + super.Industry);
+    }
+
+    public static void main(String[] args) {
+        SubQ1 q1 = new SubQ1("Cinema");
+        q1.Msg();
+    }
 }
